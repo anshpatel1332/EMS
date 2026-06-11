674408d65111f5ea458ff4e59efbc342e468e515
Ansh Patel <23bt04088@gsfcuniversity.ac.in>
2026-06-11 18:45:36 +0530

    feat: improve toggler attributes on EmployeeDashboard navbar

--- ems-frontend/src/pages/EmployeeDashboard.jsx
+++ ems-frontend/src/pages/EmployeeDashboard.jsx
@@ -49,28 +49,38 @@ function EmployeeDashboard() {
   return (
     <div className="dashboard-container">
 
-      <nav className="navbar navbar-expand-lg admin-navbar px-4">
+      <nav className="navbar navbar-expand-lg navbar-dark admin-navbar px-4">
         <div className="container-fluid">
           <h3 className="text-white fw-bold m-0">Employee Panel</h3>
-          <button className="navbar-toggler" type="button"
+
+          <button
+            className="navbar-toggler"
+            type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarContent"
             aria-controls="navbarContent"
             aria-expanded="false"
-            arial-label="toggle-navigation">
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end">
-            <div className=" d-flex flex-column flex-lg-row gap-2 mt-3 mt-lg-0">
+
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarContent"
+          >
+            <div className="d-flex flex-column flex-lg-row gap-2 mt-3 mt-lg-0">
               <button className="btn nav-btn" onClick={() => navigate("/mytasks")}>
                 My Tasks
               </button>
+
               <button
                 className="btn btn-success"
                 onClick={() => navigate("/attendance")}
               >
                 📍 Mark Attendance
               </button>
+
               <button
                 className="btn btn-primary"
                 onClick={() => navigate("/leave")}
@@ -78,6 +88,7 @@ function EmployeeDashboard() {
               >
                 🌴 Leave
               </button>
+
               <button
                 className="btn btn-danger"
                 onClick={() => {
@@ -88,7 +99,8 @@ function EmployeeDashboard() {
                 Logout
               </button>
             </div>
-          </div></div>
+          </div>
+        </div>
       </nav>
 
       <div className="container py-4">
